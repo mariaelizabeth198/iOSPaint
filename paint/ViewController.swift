30a11d2194933e357291ab81101b4e4756db6c1d
mariab <mariab@ciandt.com>
2019-02-05 10:26:32 -0200

    Initial Commit

---
+++ paint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paint
+//
+//  Created by Maria Elizabeth Da Silva Bezerra on 05/02/2019.
+//  Copyright © 2019 Maria Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
